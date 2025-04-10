85d2c6bc3c3f18e3d92bd6282e55418771d2789c
BioHome <bigjarik@gmail.com>
2025-04-10 22:31:56 +0300

    BackButton override

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -6,5 +6,20 @@
         {
             InitializeComponent();
         }
+        protected override bool OnBackButtonPressed()
+        {
+            var z = Shell.Current.CurrentPage;
+            switch (z.GetType().Name)
+            {
+                case "DimPage":
+                case "RGB_Page":
+                    Shell.Current.GoToAsync($"//List", true);
+                    return true;
+                case "ListDevPage":
+                default:
+                    return base.OnBackButtonPressed();
+                    
+            }
+        }
     }
 }
